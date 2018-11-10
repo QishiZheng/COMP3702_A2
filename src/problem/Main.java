33493ab6220d765ba4fbafa115ca61061310b476
Qishi Zheng <vincezheng4265@gmail.com>
2018-11-10 17:26:22 +1000

    added code for computing time and memory usage of the program;

--- src/problem/Main.java
+++ src/problem/Main.java
@@ -1,20 +1,46 @@
 package problem;
 
+import simulator.Simulator;
+import simulator.State;
+
 import java.io.IOException;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        //the start time of the program
+        long startTime = System.currentTimeMillis();
+        //the memory before running
+        long beforeUsedMem=Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
 
-    public static void main(String[] args) {
-
-        ProblemSpec ps;
-        try {
-            ps = new ProblemSpec("examples/level_1/input_lvl1.txt");
-            System.out.println(ps.toString());
-        } catch (IOException e) {
-            System.out.println("IO Exception occurred");
-            System.exit(1);
+        String inputFile = args[0];
+        String outputFile = args[1];
+        System.out.println(inputFile + "\n");
+        ProblemSpec ps = new ProblemSpec(inputFile);
+        simulator.Simulator sim = new Simulator(ps, outputFile);
+        DecisionMaker dm = new DecisionMaker(ps);
+        State state = sim.reset();
+        Action action;
+        while (state != null) {
+            action = dm.getAction(state);
+            //System.out.println(action.getActionType());
+            state = sim.step(action);
+            if (sim.isGoalState(state)) {
+                System.out.println("GOOOOOOAAAAAL!!\n");
+                break;
+            }
         }
-        System.out.println("Finished loading!");
+        if (state == null) {
+            System.out.println("Failure :(\n");
+        }
 
+        //elapsed time
+        long endTime = System.currentTimeMillis();
+        long timeElapsed = endTime - startTime;
+        System.out.println("Execution time in milliseconds: " + timeElapsed + "\n");
+
+        //memory used
+        long afterUsedMem=Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
+        long actualMemUsed=afterUsedMem-beforeUsedMem;
+        System.out.println("Memory Used: " + actualMemUsed/1000 + "KB");
     }
 }
